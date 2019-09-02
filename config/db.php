7ee7148925b869feb1a560c38e034033b0bfec5f
Ewl <49193020+ewlmalonzo@users.noreply.github.com>
2019-09-03 00:01:09 +0800

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-	//Create Connection
-	require('config.php');
-	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-	//Check Connection
-	if(mysqli_connect_errno()){
-		echo 'failed to connect to MySQL'.mysqli_connect_errno();
-	}
-?>
